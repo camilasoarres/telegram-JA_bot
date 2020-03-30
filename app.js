89a42a4facaa2b804e29d46d3c33ed093bf1e6f5
Camila <camila.soarees19@gmail.com>
2020-03-30 10:16:29 -0300

    remove api request and create a local array with names

--- app.js
+++ app.js
@@ -1,31 +1,230 @@
 'use strict'
-// const request = require('request');
-// const port = 'http://localhost:3001';
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 8000;
-// }
-// app.listen(port);
 const Telegram = require('telegram-node-bot');
 const TelegramBaseController = Telegram.TelegramBaseController;
 const TextCommand = Telegram.TextCommand;
 const chatbot = new Telegram.Telegram('1141948294:AAEuLFBUlpdgU-TumebDzb87JqAOUHtcErM');
 
+const persons = [
+  {
+    name: 'off: camila?',
+  },
+  {
+    name: 'off: Zoë Kravitz?',
+  },
+  {
+    name: 'off: dede?',
+  },
+  {
+    name: 'off: Jamie Dornan?'
+  },
+  {
+    name: 'off: mat?',
+  },
+  {
+    name: 'off: Henry Cavill?',
+  },
+  {
+    name: 'off: roger?',
+  },
+  {
+    name: 'off: Lula?',
+  },
+  {
+    name: 'off: Dua Lipa?',
+  },
+  {
+    name: 'off: arthur?',
+  },
+  {
+    name: 'off: Pabllo Vittar?',
+  },
+  {
+    name: 'off: thaina?',
+  },
+  {
+    name: 'off: bailey may?',
+  },
+  {
+    name: 'off: lucas?',
+  },
+  {
+    name: 'off: Idris Elba?'
+  },
+  {
+    name: 'off: mathews?',
+  },
+  {
+    name: 'off: bea de manaus?',
+  },
+  {
+    name: 'off: dana?',
+  },
+  {
+    name: 'off: David Beckham?'
+  },
+  {
+    name: 'off: bernardo?',
+  },
+  {
+    name: 'off: vic fã de kpop?',
+  },
+  {
+    name: 'off: Carlisle Cullen?',
+  },
+  {
+    name: 'off: ana paula?',
+  },
+  {
+    name: 'off: Madonna?',
+  },
+  {
+    name: 'off: tabata?',
+  },
+  {
+    name: 'off: milene?',
+  },
+  {
+    name: 'off: Ryan Gosling?',
+  },
+  {
+    name: 'off: quezinha?',
+  },
+  {
+    name: 'off: marco?',
+  },
+  {
+    name: 'off: Ryan Reynolds?',
+  },
+  {
+    name: 'off: gabi?',
+  },
+  {
+    name: 'off: Robert Pattinson?',
+  },
+  {
+    name: 'off: brenda?',
+  },
+  {
+    name: 'off: Sebastian Stan?',
+  },
+  {
+    name: 'off: bea fã do miranha?',
+  },
+  {
+    name: 'off: Bradley Cooper?',
+  },
+  {
+    name: 'off: bea larry shipper?',
+  },
+  {
+    name: 'off: marcelo?',
+  },
+  {
+    name: 'off: Jake Gyllenhaal?',
+  },
+  {
+    name: 'off :joão?',
+  },
+  {
+    name: 'off: Ivete Sangalo?',
+  },
+  {
+    name: 'off: carol?',
+  },
+  {
+    name: 'off: Pharrell Williams?',
+  },
+  {
+    name: 'off: bibika?',
+  },
+  {
+    name: 'off: Jason Momoa?',
+  },
+  {
+    name: 'off: julia?',
+  },
+  {
+    name: 'off: Hugh Jackman?',
+  },
+  {
+    name: 'off: roberta?',
+  },
+  {
+    name: 'off: Zac Efron?',
+  },
+  {
+    name: 'off: George Clooney?',
+  },
+  {
+    name: 'off: Harry Judd?',
+  },
+  {
+    name: 'off: Gary Barlow?',
+  },
+  {
+    name: 'off: Gerard Butler?',
+  },
+  {
+    name: 'off: James Franco?',
+  },
+  {
+    name: 'off: Taylor Lautner?',
+  },
+  {
+    name: 'off: Brad Pitt?',
+  },
+  {
+    name: 'off: Orlando Bloom?',
+  },
+  {
+    name: 'off: Justin Timberlake?',
+  },
+  {
+    name: 'off: Olly Murs?',
+  },
+  {
+    name: 'off: Tinie Tempah?',
+  },
+  {
+    name: 'off: Anthony Joshua?',
+  },
+  {
+    name: 'off: bruno?',
+  },
+  {
+    name: 'off: victor?',
+  },
+  {
+    name: 'off: thay?',
+  },
+  {
+    name: 'off: luísa?',
+  },
+  {
+    name: 'off: millie?',
+  },
+  {
+    name: 'off: Faustão?',
+  },
+  {
+    name: 'off: julia conífera?',
+  },
+  {
+    name: 'on: o bot?',
+  },
+]
 
 class EventsController extends TelegramBaseController {
   fariamAction(scope) {
-    // let pathApi = '/fariam'
-    let msg = 'oie'
+    let msg = '';
+    let min = 0;
+    let max = 69;
+    let random = Math.floor(Math.random() * (max - min)) + min;
 
-    // request.get(`${port}${pathApi}`, (error, response, body) => {
-    //   let min = 0;
-    //   let max = 67;
-    //   let random = Math.floor(Math.random() * (max - min)) + min;
-    //   msg += JSON.parse(body)[random].name;
-    //   scope.sendMessage(msg);
-    // })
+    msg += persons[random].name;
+
     scope.sendMessage(msg);
-  } 
+  }
   get routes() {
     return {
       'fariam': 'fariamAction'
